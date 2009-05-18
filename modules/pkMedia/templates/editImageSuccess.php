eb0761a36e08d67d1f5f4293e39ef19aad1423c5
boutell <boutell@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-05-18 15:33:12 -0500

    Fixed reference to $mediaItem that should have been $item
    git-svn-id: http://svn.symfony-project.com/plugins/pkMediaPlugin/trunk@18420 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/pkMedia/templates/editImageSuccess.php
+++ modules/pkMedia/templates/editImageSuccess.php
@@ -4,7 +4,7 @@
 	<?php // Rick and I wanted to put the breadcrumb and the browser back because this page felt naked and out of place without it.
 				// I know it's not functionally relavent to the task at hand, but visually it's less startling to move from the upload page to this page now. ?>
 
-	<?php include_component('pkMedia', 'breadcrumb', array("item" => $mediaItem)) ?>
+	<?php include_component('pkMedia', 'breadcrumb', array("item" => $item)) ?>
 	<?php include_component('pkMedia', 'browser') ?>
 
 <?php include_partial('pkMedia/editImage', 
