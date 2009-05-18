7dfd90102fffeb8714e0ca6c49f57c8c4b97d6a6
johnnyoffline <johnnyoffline@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-05-18 11:22:48 -0500

    editImageSuccess
    git-svn-id: http://svn.symfony-project.com/plugins/pkMediaPlugin/trunk@18406 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/pkMedia/templates/editImageSuccess.php
+++ modules/pkMedia/templates/editImageSuccess.php
@@ -1,4 +1,11 @@
 <?php use_helper('jQuery') ?>
 <?php slot('body_class') ?>pk-media<?php end_slot() ?>
+
+	<?php // Rick and I wanted to put the breadcrumb and the browser back because this page felt naked and out of place without it.
+				// I know it's not functionally relavent to the task at hand, but visually it's less startling to move from the upload page to this page now. ?>
+
+	<?php include_component('pkMedia', 'breadcrumb', array("item" => $mediaItem)) ?>
+	<?php include_component('pkMedia', 'browser') ?>
+
 <?php include_partial('pkMedia/editImage', 
   array('item' => $item, 'firstPass' => false, 'form' => $form)) ?>
