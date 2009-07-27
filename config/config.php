c36bbfd4c3963371c3e3a7d08d618c08b689a430
agilbert <agilbert@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-07-27 11:58:52 -0500

    Requirments patch that allows periods in media tags.
    git-svn-id: http://svn.symfony-project.com/plugins/pkMediaPlugin/trunk@20528 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- config/config.php
+++ config/config.php
@@ -3,5 +3,5 @@
 if (sfConfig::get('app_pk_media_plugin_routes_register', true) && in_array('pkMedia', sfConfig::get('sf_enabled_modules', array())))
 {
   $this->dispatcher->connect('routing.load_configuration', array('pkMediaRouting', 'listenToRoutingLoadConfigurationEvent'));
-  }
+}
 
--- lib/pkMediaRouting.php
+++ lib/pkMediaRouting.php
@@ -5,25 +5,34 @@ class pkMediaRouting
   static public function listenToRoutingLoadConfigurationEvent(sfEvent $event)
   {
     $r = $event->getSubject();
+    
     if (pkMediaTools::getOption("routes_register") && in_array('pkMedia', sfConfig::get('sf_enabled_modules')))
     {
       // NEW IN 0.5: media_items is now a subfolder of /uploads by default
       // because /uploads is world-writable by default in Symfony projects,
       // which removes configuration steps
 
-      $r->prependRoute('pk_media_image_show',
-        new sfRoute('/media/view/:slug',
-          array('module' => 'pkMedia', 'action' => 'show'),
-          array('slug' => '^[\w\-]+$')));
-      $r->prependRoute('pk_media_image_original',
-        new sfRoute('/uploads/media_items/:slug.original.:format',
-          array('module' => 'pkMedia', 'action' => 'original'),
-          array('slug' => '^[\w\-]+$', 'format' => '^(jpg|png|gif)$')));
-      $r->prependRoute('pk_media_image',
-        new sfRoute('/uploads/media_items/:slug.:width.:height.:resizeType.:format',
-          array('module' => 'pkMedia', 'action' => 'image'),
-          array('slug' => '^[\w\-]+$', 'width' => '^\d+$', 'height' => '^\d+$',
-            'resizeType' => '^\w$', 'format' => '^(jpg|png|gif)$')));
+      $r->prependRoute('pk_media_image_show', new sfRoute('/media/view/:slug', array(
+        'module' => 'pkMedia',
+        'action' => 'show'
+      ), array('slug' => '^[\w\-]+$')));
+      
+      $r->prependRoute('pk_media_image_original', new sfRoute('/uploads/media_items/:slug.original.:format', array(
+        'module' => 'pkMedia',
+        'action' => 'original'
+      ), array('slug' => '^[\w\-]+$', 'format' => '^(jpg|png|gif)$')));
+      
+      $r->prependRoute('pk_media_image', new sfRoute('/uploads/media_items/:slug.:width.:height.:resizeType.:format', array(
+        'module' => 'pkMedia',
+        'action' => 'image'
+      ), array(
+        'slug' => '^[\w\-]+$',
+        'width' => '^\d+$',
+        'height' => '^\d+$',
+        'resizeType' => '^\w$',
+        'format' => '^(jpg|png|gif)$'
+      )));
+      
       // What we want:
       // /media   <-- everything
       // /media/image   <-- media of type image
@@ -34,42 +43,62 @@ class pkMediaRouting
       // /media?search=blah blah blah  <-- searches are full of
       //                                   dirty URL-unfriendly characters and
       //                                   are traditionally query strings.
-      $r->prependRoute('pk_media_index',
-        new sfRoute('/media',
-          array('module' => 'pkMedia', 'action' => 'index')));
-      $r->prependRoute('pk_media_index_type',
-        new sfRoute('/media/:type',
-          array('module' => 'pkMedia', 'action' => 'index'),
-          array('type' => '(image|video)')));
-      $r->prependRoute('pk_media_index_tag',
-        new sfRoute('/media/tag/:tag',
-          array('module' => 'pkMedia', 'action' => 'index')));
-      $r->prependRoute('pk_media_index_type_tag',
-        new sfRoute('/media/:type/tag/:tag',
-          array('module' => 'pkMedia', 'action' => 'index'),
-          array('type' => '(image|video)')));
+      
+      $r->prependRoute('pk_media_index', new sfRoute('/media', array(
+        'module' => 'pkMedia', 
+        'action' => 'index'
+      )));
+      
+      $r->prependRoute('pk_media_index_type', new sfRoute('/media/:type', array(
+        'module' => 'pkMedia',
+        'action' => 'index'
+      ), array('type' => '(image|video)')));
+      
+      $r->prependRoute('pk_media_index_tag', new sfRoute('/media/tag/:tag', array(
+        'module' => 'pkMedia',
+        'action' => 'index'
+      ), array('tag' => '.*')));
+            
+      $r->prependRoute('pk_media_index_type_tag', new sfRoute('/media/:type/tag/:tag', array(
+        'module' => 'pkMedia',
+        'action' => 'index'
+      ), array('type' => '(image|video)', 'tag' => '.*')));
+      
       // APIs
-      $r->prependRoute('pk_media_select',
-        new sfRoute('/media/select',
-          array('module' => 'pkMedia', 'action' => 'select')));
-      $r->prependRoute('pk_media_info',
-        new sfRoute('/media/info',
-          array('module' => 'pkMedia', 'action' => 'info')));
-      $r->prependRoute('pk_media_tags',
-        new sfRoute('/media/tags',
-          array('module' => 'pkMedia', 'action' => 'tags')));
-      $r->prependRoute('pk_media_upload_images',
-        new sfRoute('/media/uploadImages',
-          array('module' => 'pkMedia', 'action' => 'uploadImages')));
-      $r->prependRoute('pk_media_edit_images',
-        new sfRoute('/media/editImages',
-          array('module' => 'pkMedia', 'action' => 'editImages')));
-      $r->prependRoute('pk_media_new_video',
-        new sfRoute('/media/newVideo',
-          array('module' => 'pkMedia', 'action' => 'newVideo')));
-      $r->prependRoute('pk_media_edit_video',
-        new sfRoute('/media/editVideo',
-          array('module' => 'pkMedia', 'action' => 'editVideo')));
+      $r->prependRoute('pk_media_select', new sfRoute('/media/select', array(
+        'module' => 'pkMedia',
+        'action' => 'select'
+      )));
+      
+      $r->prependRoute('pk_media_info', new sfRoute('/media/info', array(
+        'module' => 'pkMedia',
+        'action' => 'info'
+      )));
+      
+      $r->prependRoute('pk_media_tags', new sfRoute('/media/tags', array(
+        'module' => 'pkMedia',
+        'action' => 'tags'
+      )));
+      
+      $r->prependRoute('pk_media_upload_images', new sfRoute('/media/uploadImages', array(
+        'module' => 'pkMedia',
+        'action' => 'uploadImages'
+      )));
+      
+      $r->prependRoute('pk_media_edit_images', new sfRoute('/media/editImages', array(
+        'module' => 'pkMedia',
+        'action' => 'editImages'
+      )));
+      
+      $r->prependRoute('pk_media_new_video', new sfRoute('/media/newVideo', array(
+        'module' => 'pkMedia',
+        'action' => 'newVideo'
+      )));
+      
+      $r->prependRoute('pk_media_edit_video', new sfRoute('/media/editVideo', array(
+        'module' => 'pkMedia',
+        'action' => 'editVideo'
+      )));
     }
   }
 }
