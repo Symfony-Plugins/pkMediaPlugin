103ccb9f820e6c5f79a040fa00a600058273e486
agilbert <agilbert@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-10-21 15:53:01 -0500

    Improved readability
    git-svn-id: http://svn.symfony-project.com/plugins/pkMediaPlugin/trunk@23237 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/form/pkMediaEditImagesForm.class.php
+++ lib/form/pkMediaEditImagesForm.class.php
@@ -3,11 +3,13 @@
 class pkMediaEditImagesForm extends sfForm
 {
   private $active = array();
+  
   public function __construct($active)
   {
     $this->active = array_flip($active);
     parent::__construct();
   }
+  
   public function configure()
   {
     for ($i = 0; ($i < pkMediaTools::getOption('batch_max')); $i++)
@@ -17,6 +19,7 @@ class pkMediaEditImagesForm extends sfForm
         $this->embedForm("item-$i", new pkMediaImageForm());
       }
     }
+    
     $this->widgetSchema->setNameFormat('pk_media_items[%s]'); 
   }
 }
--- lib/form/pkMediaImageForm.class.php
+++ lib/form/pkMediaImageForm.class.php
@@ -10,43 +10,44 @@ class pkMediaImageForm extends pkMediaItemForm
     unset($this['width']);
     unset($this['height']);
     unset($this['format']);
-    $this->setWidget('file', 
-      new pkWidgetFormInputFilePersistent(
-        array(
-          // Not yet
-          // "iframe" => true,
-          // "progress" => "Uploading...",
-          "image-preview" => pkMediaTools::getOption('gallery_constraints'))));
-    $this->setValidator('file', new pkValidatorFilePersistent(
-      array("mime_types" => array("image/jpeg", "image/png", "image/gif"), 
-        "required" => (!$this->getObject()->getId())),
-      array("mime_types" => "JPEG, PNG and GIF only.",
-        "required" => "Select a JPEG, PNG or GIF file")));
-    // These have to be brief to work with Rick's styles
-    $this->setValidator('title', 
-      new sfValidatorString(
-        array("min_length" => 3, "max_length" => 200, "required" => true),
-        array("min_length" => "Title must be at least 3 characters.",
-          "max_length" => "Title must be <200 characters.",
-          "required" => "You must provide a title.")));
+    
+    $this->setWidget('file', new pkWidgetFormInputFilePersistent(array(
+      // Not yet
+      // "iframe" => true,
+      // "progress" => "Uploading...",
+      'image-preview' => pkMediaTools::getOption('gallery_constraints')
+    )));
 
-		$this->setWidget('view_is_secure', new sfWidgetFormChoice(
-			array(
-				'expanded' => true,
-			  'choices' => array(
-				0 => "Public",
-				1 => "Hidden"
-				),
-				'default' => 0
-				)));
+    $this->setValidator('file', new pkValidatorFilePersistent(array(
+      'mime_types' => array('image/jpeg', 'image/png', 'image/gif'), 
+      'required' => (!$this->getObject()->getId())
+    ), array(
+      'mime_types' => 'JPEG, PNG and GIF only.',
+      'required' => 'Select a JPEG, PNG or GIF file')
+    ));
+    
+    $this->setValidator('title', new sfValidatorString(array(
+      'min_length' => 3,
+      'max_length' => 200,
+      'required' => true
+    ), array(
+      'min_length' => 'Title must be at least 3 characters.',
+      'max_length' => 'Title must be <200 characters.',
+      'required' => 'You must provide a title.')
+    ));
+
+		$this->setWidget('view_is_secure', new sfWidgetFormChoice(array(
+		  'expanded' => true,
+		  'choices' => array(0 => 'Public', 1 => 'Hidden'),
+		  'default' => 0
+		)));
 	
-  		$this->setValidator('view_is_secure', new sfValidatorBoolean());
+		$this->setValidator('view_is_secure', new sfValidatorBoolean());
 
-    $this->widgetSchema->setLabel("view_is_secure", "Permissions");
+    $this->widgetSchema->setLabel('view_is_secure', 'Permissions');
     $this->widgetSchema->setNameFormat('pk_media_item[%s]');
-
-	
   }
+  
   public function updateObject($values = null)
   {
     $object = parent::updateObject($values);
